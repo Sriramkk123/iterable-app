43e86957c4ca49b7adc9b44ac925ed63365cffaf
Sriram K K <sriram@beskar.tech>
2023-10-12 18:05:10 +0530

    Refactor controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,25 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :initialize_iterable_io_service
 
   def web_push
-    iterable_io_service = IterableIoService.new("dummy_key")
-    @user = current_user
-    @event = @user.events.create({ name: 'web push', event_type: :web_push })
-    iterable_io_service.create_web_push_event(@user, @event)
+    create_event(:web_push)
+    @iterable_io_service.create_web_push_event(@user, @event)
   end
 
   def mobile_push
-    iterable_io_service = IterableIoService.new("dummy_key")
+    create_event(:mobile_push)
+    @iterable_io_service.create_mobile_push_event(@user, @event)
+  end
+
+  private
+
+  def initialize_iterable_io_service
+    @iterable_io_service = IterableIoService.new("dummy_key")
+  end
+
+  def create_event(event_type)
     @user = current_user
-    @event = @user.events.create({ event_type: :mobile_push })
-    iterable_io_service.create_mobile_push_event(@user, @event)
+    @event = @user.events.create({ name: event_type, event_type: event_type })
   end
 
 end
